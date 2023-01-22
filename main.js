8c4fab2f9fb0cc9330c521687cee3437d8d37255
ChaitanyaNariyalwale <88239191+ChaitanyaNariyalwale@users.noreply.github.com>
2023-01-22 14:48:40 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,35 +1,54 @@
-Webcam.attach( '#camera' );
-camera = document.getElementById("camera"); 
-Webcam.set({ width:350, height:300, image_format : 'png', png_quality:90 });
-
-function take_snapshot()
-{
-    Webcam.snap(function(data_uri){
-        document.getElementById("result").innerHTML='<img id="selfie_image" src="'+data_uri+'"/>';
-    });
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio:true});
+    classifier=ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/kA4F-80yW/model.json',modelReady);
 }
-
-console.log('ml5 version:',ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/JACkTMciY/model.json',modelLoaded);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json',modelLoaded);
-
-function modelLoaded(){
-    console.log('model Loaded');
+function modelReady(){
+    classifier.classify(gotResults);
 }
-
-function check(){
-    img = document.getElementById('selfie_image');
-    classifier.classify(img,gotResult);
-}
-
-function gotResult(error,results){
-    if (error){
+function gotResults(error,results){
+    if(error){
         console.error(error);
     }
     else{
         console.log(results);
-        document.getElementById("result_object_name").innerHTML=results[0].label;
-        document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(3);
-    }
-}
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
 
+        document.getElementById("result_label").innerHTML = 'I can hear - '+ results[0].label;
+        document.getElementById("result_confidence").innerHTML = 'accuracy - '+ (results[0].confidence*100).toFixed(2)+"%";
+        document.getElementById("result_label").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+        document.getElementById("result_confidence").style.color = "rgb("+random_number_r+","+random_number_g+","+random_number_r+")";
+
+        img=document.getElementById('alien1')
+        img1=document.getElementById('alien2')
+        img2=document.getElementById('alien3')
+        img3=document.getElementById('alien4')
+
+        if(results[0].label=="clap"){
+            img.src='aliens-01.gif';
+            img1.src='aliens-02.png';
+            img2.src='aliens-03.png';
+            img3.src='aliens-04.png';
+        }
+        else if(results[0].label=="bell"){
+            img.src='aliens-01.png';
+            img1.src='aliens-02.gif';
+            img2.src='aliens-03.png';
+            img3.src='aliens-04.png';
+        }
+        else if(results[0].label=="snap"){
+            img.src='aliens-01.png';
+            img1.src='aliens-02.png';
+            img2.src='aliens-03.gif';
+            img3.src='aliens-04.png';
+        }
+        else {
+            img.src='aliens-01.png';
+            img1.src='aliens-02.png';
+            img2.src='aliens-03.png';
+            img3.src='aliens-04.gif';
+        }
+    }
+
+}
